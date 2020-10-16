a3f2cdf2fd8f67b86ddb8f87a2d8042b5463cc2b
Luis Carpio <luisdcm18@gmail.com>
2020-10-15 20:05:17 -0500

    primer patron

---
+++ practica.js
@@ -0,0 +1,13 @@
+class Comidas{
+    constructor(precio,cantidad){
+        this.precio=precio;
+        this.cantidad=cantidad;
+        this.cuenta=function pedido(){
+             return this.cantidad*this.precio;
+        }
+    }
+    
+}
+
+let ceviche=new Comidas(14.5,2);
+console.log(ceviche.cuenta());
